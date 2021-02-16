19c2a6ece2fdf324608b85a3bf84086269b1cc5d
Joanna Brigham <joannabrigham@hotmail.co.uk>
2021-02-16 15:39:07 +0100

    update cat to random cat and add named cat

--- app.rb
+++ app.rb
@@ -10,8 +10,13 @@ get '/secret' do
   "Norman and Ronnie rule the world"
 end
 
-get '/cat' do
-  "<div style='border: 3px dashed red'>
-  <img src='https://cnet2.cbsistatic.com/img/n6FwSNUEI6P-zYtzAXegHFGjxeM=/940x0/2021/02/09/62a82602-5227-4b0c-875c-302af79cc49e/zoomcat.jpg'>
-  </div>"
+get '/random-cat' do
+  @name = ["Amigo", "Misty", "Almond"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
+  erb(:index)
 end
